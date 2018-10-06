ce1f03434eb8d66fa9c3d834ade262517dbd6fa1
Ka Chan <luandeka@gmail.com>
2018-10-06 07:55:13 -0400

    shh
    Just small QoL changes.

--- src/main/java/com/github/decyg/MainRunner.java
+++ src/main/java/com/github/decyg/MainRunner.java
@@ -43,7 +43,6 @@ public class MainRunner {
 				}
 				catch (DiscordException e)
 				{
-					IDiscordClient thisclient = BotUtils.getBuiltDiscordClient("MjA1Mzk1MTQwNTI3NzgzOTU3.DA5F_w.7h5vrV6EpWLhAbEUZU-tB56v-to");
 					thisclient.logout();
 					thisclient.login();
 					System.out.println("-------------restarted-------------");
